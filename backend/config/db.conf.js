8d720a374d6d132687714e9eba8af801f3a49f48
shaw342 <shawanbarua70@gmail.com>
2025-03-08 19:19:24 +0100

    update: implement connection verification end change parameter sequelize

--- backend/config/db.conf.js
+++ backend/config/db.conf.js
@@ -1,10 +1,21 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize({
-    database:process.env.DB,
-    host:process.env.HOST,
-    password:process.env.PASSWORD,
-    username:process.env.USERNAME
+const sequelize = new Sequelize(process.env.DB,process.env.USERNAME,process.env.PASSWORD,{
+  host:"localhost",
+  dialect:"mysql"
 })
+
+try {
+
+  await sequelize.authenticate()
+  console.log('====================================');
+  console.log("successful to login in database");
+  console.log('====================================');
+  
+} catch (error) {
+  console.log('====================================');
+  console.log(error);
+  console.log('====================================');
+}
 
 module.exports = sequelize
